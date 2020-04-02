b732bff59cdc3fdb155a4e52693462bc080d1edc
yafanisonya <yafanison@gmail.com>
2020-04-02 10:09:59 +0800

    使用parcel，重写 button.vue

---
+++ src/button.vue
@@ -0,0 +1,32 @@
+<template>
+  <button class="g-button">按钮</button>
+</template>
+
+<script lang="ts">
+  import Vue from 'vue';
+  import {Component} from 'vue-property-decorator';
+
+  @Component
+  export default class button extends Vue {
+  }
+</script>
+
+<style lang="scss" scoped>
+  .g-button{
+    height: var(--button-height);
+    font-size: var(--font-size);
+    padding:0 1em;
+    border-radius:var(--border-radius);
+    border:1px solid var(--border-color);
+    background: var(--button-bg);
+    &:hover{
+      border-color:var(--border-color-hover);
+    }
+    &:active{
+      background-color: var(--button-active-bg);
+    }
+    &:focus{
+      outline:none;
+    }
+  }
+</style>
