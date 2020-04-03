14cdff245476ea74fd635ddd8f74601bd38f5ba3
yafanisonya <yafanison@gmail.com>
2020-04-03 08:55:04 +0800

    重新添加loading状态

--- src/button.vue
+++ src/button.vue
@@ -1,28 +1,20 @@
 <template>
-  <button class="g-button" :class="{[`icon-${iconPosition}`]:true}">
-    <g-icon class="icon" v-if="icon" :name="icon"/>
-    <g-icon class="loading" name="loading"/>
+  <button class="g-button" :class="{[`icon-${iconPosition}`]: true}">
+    <g-icon class="icon" v-if="icon" :name="icon"></g-icon>
+    <g-icon class="loading" name="loading"></g-icon>
     <div class="content">
-      <slot/>
+      <slot></slot>
     </div>
   </button>
 </template>
-
 <script>
   export default {
     //props:['icon','iconPosition']
     props:{
       icon:{},
       iconPosition:{
-        type: String,
-        default: 'left',
-        // validator(value){
-        //   if(value !== 'left' && value !== 'right'){
-        //     return false
-        //   }else{
-        //     return true
-        //   }
-        // }
+        type:String,
+        default:'left',
         validator(value){
           return value === 'left' || value === 'right'
         }
@@ -31,20 +23,23 @@
   }
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
   @keyframes spin {
-    0% { transform: rotate(0deg);}
-    50% { transform: rotate(180deg);}
-    100% { transform: rotate(360deg);}
-  }
-  .loading{
-    animation: spin 3s infinite linear;
+    0% {
+      transform: rotate(0deg);
+    }
+    50% {
+      transform: rotate(180deg);
+    }
+    100% {
+      transform: rotate(360deg);
+    }
   }
   .g-button{
-    height: var(--button-height);
-    font-size: var(--font-size);
+    font-size:var(--font-size);
+    height:var(--button-height);
     padding:0 1em;
-    border-radius:var(--border-radius);
+    border-radius: var(--border-radius);
     border:1px solid var(--border-color);
     background: var(--button-bg);
     display: inline-flex;
@@ -52,7 +47,7 @@
     align-items: center;
     vertical-align: middle;
     &:hover{
-      border-color:var(--border-color-hover);
+      border-color: var(--border-color-hover);
     }
     &:active{
       background-color: var(--button-active-bg);
@@ -60,12 +55,39 @@
     &:focus{
       outline:none;
     }
-    > .content{ order: 2;}
-    > .icon { order:1; margin-right: .1em;}
+    > .content{
+      order:2;
+    }
+    > .icon{
+      order:1;
+      margin-right: .1em;
+    }
 
     &.icon-right{
       > .content{ order:1;}
-      > .icon { order:2; margin-right:0; margin-left:.1em;}
+      > .icon{ order:2;margin-right:0;
+        margin-left: .1em;}
+    }
+    .loading{
+      animation: spin 3s infinite linear;
     }
   }
 </style>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
