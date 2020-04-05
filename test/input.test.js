0ba91643b22668f3aae602f4155f10a0b4d1e743
yafanisonya <yafanison@gmail.com>
2020-04-05 09:03:39 +0800

    input add v-model

--- test/input.test.js
+++ test/input.test.js
@@ -68,9 +68,14 @@ describe('Input',()=>{
         vm = new Constructor({}).$mount;
         vm.$on(eventName, callback)
         let event = new Event(eventName);
+        Object.defineProperty(
+          event,'target',{
+            value:{value:'hi'},enumerable:true
+          }
+        )
         let inputElement = vm.$el.querySelector('input')
         inputElement.dispatchEvent(event)
-        expect(callback).to.have.been.calledWith(event)
+        expect(callback).to.have.been.calledWith('hi')
       })
     })
   })
