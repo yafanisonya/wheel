143f65e76e6b4f3d9ae80644450b9e2c380c46cb
yafanisonya <yafanison@gmail.com>
2020-04-09 09:41:26 +0800

    添加 tab组件

---
+++ src/tabs-head.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="tabs-head">
+    <slot></slot>
+    <slot name="actions"></slot>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'WheelTabsHead'
+  }
+</script>
+
+<style>
+  .tabs-head{
+
+  }
+</style>
---
+++ src/tabs-pane.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="tabs-pane">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+  export default {
+    name:'WheelTabsPane'
+  }
+</script>
+
+<style>
+  .tabs-pane{
+
+  }
+</style>
