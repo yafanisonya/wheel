4d2ee1fd3b3c5f6d5140eda7d68c26f2291426e4
yafanisonya <yafanison@gmail.com>
2020-04-09 22:53:25 +0800

    设置默认 selected

--- src/app.js
+++ src/app.js
@@ -51,7 +51,8 @@ new Vue({
     loading2:false,
     loading3:false,
     message:'hi',
-    selectedTab:'sports'
+    selectedTab:'sports',
+    selectedColTab:['1','2']
   },
   created(){
   },
--- src/collapse-item.vue
+++ src/collapse-item.vue
@@ -12,6 +12,10 @@
       title:{
         type: String,
         required: true
+      },
+      name:{
+        type: String,
+        required: true
       }
     },
     data(){
@@ -21,24 +25,22 @@
     },
     inject:['eventBus'],
     mounted(){
-      this.eventBus && this.eventBus.$on('update:selected',(vm)=>{
-        if( vm !== this){
-          this.close()
+      this.eventBus && this.eventBus.$on('update:selected',(names)=>{
+        if( names.indexOf(this.name) >= 0){
+          this.open = true
+        }else{
+          this.open = false
         }
       })
     },
     methods:{
       toggle(){
         if(this.open){
-          this.open = false
+          this.eventBus && this.eventBus.$emit('update:removeSelected',this.name)
         }else{
-          this.open = true
-          this.eventBus && this.eventBus.$emit('update:selected',this)
+          this.eventBus && this.eventBus.$emit('update:addSelected',this.name)
         }
       },
-      close(){
-        this.open = false
-      }
     }
   }
 </script>
