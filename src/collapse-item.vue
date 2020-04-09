37c20445200d107461ad308f8b58f076b3f7c7ea
yafanisonya <yafanison@gmail.com>
2020-04-09 17:41:27 +0800

    实现 collapse 基本样式功能

---
+++ src/collapse-item.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="collapseItem">
+    <div class="title" @click="toggle">{{title}}</div>
+    <div class="content" v-if="open"><slot></slot></div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name:'WheelCollapseItem',
+    props:{
+      title:{
+        type: String,
+        required: true
+      }
+    },
+    data(){
+      return{
+        open: false
+      }
+    },
+    inject:['eventBus'],
+    mounted(){
+      this.eventBus && this.eventBus.$on('update:selected',(vm)=>{
+        if( vm !== this){
+          this.close()
+        }
+      })
+    },
+    methods:{
+      toggle(){
+        if(this.open){
+          this.open = false
+        }else{
+          this.open = true
+          this.eventBus && this.eventBus.$emit('update:selected',this)
+        }
+      },
+      close(){
+        this.open = false
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  .collapseItem{
+    > .title{
+      border: 1px solid #ddd;
+      margin-top: -1px;
+      margin-left:-1px;
+      margin-right:-1px;
+      min-height: 32px;
+      display: flex;
+      align-items: center;
+      padding: 0 8px;
+    }
+    &:first-child{
+      > .title{
+        border-top-left-radius: 4px;
+        border-top-right-radius: 4px;
+      }
+    }
+    &:last-child{
+      > .title:last-child{
+        border-bottom-left-radius:4px;
+        border-bottom-right-radius:4px;
+      }
+    }
+    > .content{
+      padding: 8px;
+    }
+  }
+
+</style>
---
+++ src/collapse.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="collapse">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+  import Vue from 'vue'
+  export default{
+    name: "WheelCollapse",
+    props:{
+      single:{
+        type:Boolean,
+        default: false
+      }
+    },
+    data(){
+      return {
+        eventBus: new Vue()
+      }
+    },
+    provide(){
+      if(this.single){
+        return {
+          eventBus: this.eventBus
+        }
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  .collapse{
+    border: 1px solid #ddd;
+    border-radius: 4px;
+  }
+</style>
